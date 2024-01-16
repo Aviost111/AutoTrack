a68c7fea133e69e870cd52c0315557119097b819
aviya oren <98823130+OrenAviya@users.noreply.github.com>
2024-01-16 23:00:23 +0200

    נסיון להתחיל רשימת עובדים שתסתנכרן עם הדאטה בייס

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -30,6 +30,9 @@ android {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
     }
+    buildFeatures {
+        viewBinding = true
+    }
 }
 
 dependencies {
@@ -37,9 +40,25 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.7.0")
+    implementation("androidx.lifecycle:lifecycle-view-model-ktx:2.7.0")
+    implementation("androidx.navigation:navigation-fragment:2.7.6")
+    implementation("androidx.navigation:navigation-ui:2.7.6")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     implementation(platform("com.google.firebase:firebase-bom:32.7.0"))
     implementation("com.google.firebase:firebase-analytics")
+    implementation ("com.google.android.material:material:1.11.0")
+    implementation ("com.firebase:firebase-ui-firestorm:8.0.0")
+
+        // Other dependencies
+    implementation ("com.firebaseui:firebase-ui-database:8.0.0")
+
+
+    //add library:
+    implementation ("androidx.card-view:card-view:1.0.0")
+    implementation("androidx.recyclerview:recyclerview:1.3.2")
+    implementation ("com.square.picasso:picasso:2.5.2")
+    implementation ("com.firebase:firebase-ui-database:1.2.0")
 }
---
+++ app/src/main/java/com/example/autotrack/EmployeesList.java
@@ -0,0 +1,69 @@
+package com.example.autotrack;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import android.os.Bundle;
+import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+
+import android.os.Bundle;
+
+public class EmployeesList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_employees_list);
+
+        // Create a instance of the database and get
+        // its reference
+        mbase
+                = FirebaseDatabase.getInstance().getReference();
+
+        recyclerView = findViewById(R.id.recycler1);
+
+        // To display the Recycler view linearly
+        recyclerView.setLayoutManager(
+                new LinearLayoutManager(this));
+
+        // It is a class provide by the FirebaseUI to make a
+        // query in the database to fetch appropriate data
+        FirebaseRecyclerOptions<Employee> options
+                = new FirebaseRecyclerOptions.Builder<Employee>()
+                .setQuery(mbase, Employee.class)
+                .build();
+        // Connecting object of required Adapter class to
+        // the Adapter class itself
+        adapter = new employeeAdapter(options);
+        // Connecting Adapter class with the Recycler view*/
+        recyclerView.setAdapter(adapter);
+    }
+
+}
+
+    private RecyclerView recyclerView;
+    employeeAdapter
+            adapter; // Create Object of the Adapter class
+    DatabaseReference mbase; // Create object of the
+    // Firebase Realtime Database
+
+
+    // Function to tell the app to start getting
+    // data from database on starting of the activity
+    @Override protected void onStart()
+    {
+        super.onStart();
+        adapter.startListening();
+    }
+
+    // Function to tell the app to stop getting
+    // data from database on stopping of the activity
+    @Override protected void onStop()
+    {
+        super.onStop();
+        adapter.stopListening();
+    }
+}
