265855cf371d78e0b057f4c8a49ae6fd3eccc8e1
Noa Amichai <noa1254@gmail.com>
2024-01-23 14:14:06 +0200

    noaaaa

--- app/src/main/java/com/example/autotrack/LoginActivity.java
+++ app/src/main/java/com/example/autotrack/LoginActivity.java
@@ -4,11 +4,16 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.firebase.FirebaseApp;
+
 public class LoginActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        FirebaseApp.initializeApp(this); //TODO - remove this line before committing
+
     }
 }
