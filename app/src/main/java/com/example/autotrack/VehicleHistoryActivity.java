58a3dff6f02370890920439a80bf2fbff5066a89
Noa Amichai <noa1254@gmail.com>
2024-02-18 11:45:09 +0200

    .

--- app/src/main/java/com/example/autotrack/VehicleHistoryActivity.java
+++ app/src/main/java/com/example/autotrack/VehicleHistoryActivity.java
@@ -10,9 +10,11 @@ import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
 import android.widget.Toast;
+
 import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
+
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
@@ -55,25 +57,25 @@ public class VehicleHistoryActivity extends AppCompatActivity {
                 .document(userMail)
                 .get()
                 .addOnCompleteListener(task -> {
-                            if (task.isSuccessful()) {
-                                DocumentSnapshot document = task.getResult();
-                                if (document.exists()) {
-                                    // Access the document data
-                                    companyId = document.getString("company_id");
+                    if (task.isSuccessful()) {
+                        DocumentSnapshot document = task.getResult();
+                        if (document.exists()) {
+                            // Access the document data
+                            companyId = document.getString("company_id");
 
-                                    // Retrieve and populate treatments, refuels and start-stop data
-                                    retrieveAndPopulateData("start-stop", listViewStartStop);
-                                    retrieveAndPopulateData("treatments", listViewTreatments);
-                                    retrieveAndPopulateData("refuels", listViewRefuels);
-                                } else {
-                                    // Document doesn't exist
-                                    Log.d("Firestore", "No such document");
-                                }
-                            } else {
-                                // Task failed with an exception
-                                Log.e("Firestore", "Error getting document: ", task.getException());
-                            }
-                        });
+                            // Retrieve and populate treatments, refuels and start-stop data
+                            retrieveAndPopulateData("start-stop", listViewStartStop);
+                            retrieveAndPopulateData("treatments", listViewTreatments);
+                            retrieveAndPopulateData("refuels", listViewRefuels);
+                        } else {
+                            // Document doesn't exist
+                            Log.d("Firestore", "No such document");
+                        }
+                    } else {
+                        // Task failed with an exception
+                        Log.e("Firestore", "Error getting document: ", task.getException());
+                    }
+                });
     }
 
     private void retrieveAndPopulateData(String subcollectionName, ListView listView) {
@@ -102,37 +104,38 @@ public class VehicleHistoryActivity extends AppCompatActivity {
 
                             // Iterate over the entries of the top-level map
                             for (Map.Entry<String, Object> entry : dataMap.entrySet()) {
-                                    String key = entry.getKey();
+                                String key = entry.getKey();
 
-                                    // Parse the epoch timestamp (key) string to a long integer
-                                    long epoch = Long.parseLong(key);
+                                // Parse the epoch timestamp (key) string to a long integer
+                                long epoch = Long.parseLong(key);
 
-                                    // Create a Date object from the epoch time
-                                    Date date = new Date(epoch);
+                                // Create a Date object from the epoch time
+                                Date date = new Date(epoch);
 
-                                    // Define the date and hour format
-                                    SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+                                // Define the date and hour format
+                                SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
 
-                                    // Format the Date object to a string in the desired format
-                                    String formattedDateTime = dateFormat.format(date);
+                                // Format the Date object to a string in the desired format
+                                String formattedDateTime = dateFormat.format(date);
 
-                                    Map<String, Object> innerMap = (Map<String, Object>) entry.getValue();
+                                Map<String, Object> innerMap = (Map<String, Object>) entry.getValue();
 
-                                    // Iterate over the entries of the inner map
-                                    for (Map.Entry<String, Object> innerEntry : innerMap.entrySet()) {
-                                        // Append the field and value with appropriate formatting
-                                        data.append(innerEntry.getKey()).append(": ").append(innerEntry.getValue()).append("\n");
-                                    }
-                                    dataList.add("Date: "+formattedDateTime + "\n" + data);
-                                    // Clear the StringBuilder for the next iteration
-                                    data.setLength(0);
+                                // Iterate over the entries of the inner map
+                                for (Map.Entry<String, Object> innerEntry : innerMap.entrySet()) {
+                                    // Append the field and value with appropriate formatting
+                                    data.append(innerEntry.getKey().substring(0, 1).toUpperCase() + innerEntry.getKey().substring(1).replace("_" ," ")).append(": ").append(innerEntry.getValue()).append("\n");
                                 }
 
-                                // Populate the ListView with the retrieved data
-                                ArrayAdapter<String> adapter = new ArrayAdapter<>(this,
-                                        android.R.layout.simple_list_item_1, dataList);
-                                listView.setAdapter(adapter);
-                        } else{
+                                dataList.add("Date: " + formattedDateTime + "\n" + data);
+                                // Clear the StringBuilder for the next iteration
+                                data.setLength(0);
+                            }
+
+                            // Populate the ListView with the retrieved data
+                            ArrayAdapter<String> adapter = new ArrayAdapter<>(this,
+                                    android.R.layout.simple_list_item_1, dataList);
+                            listView.setAdapter(adapter);
+                        } else {
                             // Handle the case when the document doesn't exist
                             Toast.makeText(this, "Document does not exist", Toast.LENGTH_SHORT).show();
                         }
